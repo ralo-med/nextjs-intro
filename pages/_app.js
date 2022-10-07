42c4ffdcce91983b54fa28874642815d5fb36ed5
ralo <leona8464@gmail.com>
2022-10-07 18:26:56 +0900

    #2.0 Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
+
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </div>
+    </Layout>
   );
 }
